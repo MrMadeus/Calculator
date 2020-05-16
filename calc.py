f0fd8284415f986af6a269f82e2885f9e9a6be04
andrew.kurachkin <kurockinandrew@gmail.com>
2020-05-16 17:49:19 +0300

    ALL CHANGED

--- calc.py
+++ calc.py
@@ -1,27 +1,20 @@
 from tkinter import *
 
-class NumButton:
-    def __init__(self, master, num, field):
-        self.num = num
-        self.N = Button(master, text=str(num), width=3, height=3)
-        self.N.pack()
-        
-
-
-
-class OperationButton:
-    pass
-
 root = Tk()
 
 WorkField = Entry(bg='white', fg='black', width=40)
 def restart():
     WorkField.delete(0, END)
+def insert(num):
+    WorkField.insert(END, str(num))
 
 C = Button(text="C", width=3, height=3, command=restart)
 
-n1 = NumButton(root, 1)
-n1.PrntNum(WorkField)
+n = []
+
+for i in range(10):
+    n.append(Button(text=str(i), command=insert(i)))
+    n[i].pack(side=LEFT)
 
 WorkField.pack()
 C.pack()
