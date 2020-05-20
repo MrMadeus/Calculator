8234560d291b858266d40c76e83933e860af2814
andrew.kurachkin <kurockinandrew@gmail.com>
2020-05-20 17:32:24 +0300

    finaly made grid

--- calc.py
+++ calc.py
@@ -13,10 +13,29 @@ C = Button(text="C", width=3, height=3, command=restart)
 n = []
 
 for i in range(10):
-    n.append(Button(text=str(i), command=insert(i)))
-    n[i].pack(side=LEFT)
+    n.append(Button(text=str(i), width=3, height=3))
+    if i == 0:
+        n[i].grid(row=2, column=4)
+    elif (i%3) == 0:
+        n[i].grid(row=i//3, column=3)
+    elif (i%3) == 1:
+        n[i].grid(row=1+(i//3), column=2)
+    elif (i%3) == 2:
+        n[i].grid(row=1+(i//3), column=1)
 
-WorkField.pack()
-C.pack()
+
+n[0].bind('<Button-1>', insert(0))
+n[1].bind('<Button-1>', insert(1))
+n[2].bind('<Button-1>', insert(2))
+n[3].bind('<Button-1>', insert(3))
+n[4].bind('<Button-1>', insert(4))
+n[5].bind('<Button-1>', insert(5))
+n[6].bind('<Button-1>', insert(6))
+n[7].bind('<Button-1>', insert(7))
+n[8].bind('<Button-1>', insert(8))
+n[9].bind('<Button-1>', insert(9))
+
+WorkField.grid(row=0, column=0)
+C.grid(row=1, column=4)
 
 root.mainloop()
